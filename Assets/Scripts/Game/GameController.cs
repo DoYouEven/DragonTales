3f38f16e17c26eadf31dfb4e340fed1e0b5017e0
Jeff Jelovic <jeffjelovic@gmail.com>
2015-03-04 08:01:59 +0000

    simple win/lose

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -4,21 +4,54 @@ using System.Collections.Generic;
 
 
 
-   public class GameController : MonoBehaviour
-    {
-       public float currentTime { get; protected set; }
-       public float MaxGameTime = 60;
-       public MoveAssetDatabase moveAssetDatabase;
+public class GameController : MonoBehaviour
+{
+	public float currentTime { get; protected set; }
+	public float MaxGameTime = 60;
+	public MoveAssetDatabase moveAssetDatabase;
+	public GameObject player1, player2;
 
-       public static GameController instance;
-       void Awake()
-       {
-           instance = this;
-       }
-       void Update()
-       {
-           currentTime += Time.deltaTime;
-       }
+	public static GameController instance;
+	void Awake()
+	{
+		if(player1 == null || player2 == null)
+		{
+			Debug.Log("Please Attach players to gamecontroller");
+		}
+		instance = this;
+	}
 
-    }
+	void Start()
+	{
+		StartCoroutine (Winner ());
+	}
+
+	void Update()
+	{
+		currentTime += Time.deltaTime;
+	}
+
+	IEnumerator Winner() 
+	{
+		yield return new WaitForSeconds (MaxGameTime);
+		int p1Tails = player1.GetComponent<DragonBase> ().tails.Count;
+		int p2Tails = player2.GetComponent<DragonBase> ().tails.Count;
+
+		if (p1Tails > p2Tails) {
+			Debug.Log ("Player 1 Wins!");
+			// TODO: display who wins
+		} 
+		else if (p1Tails < p2Tails) 
+		{
+			Debug.Log ("Player 2 Wins!");
+			// TODO: display who wins	
+		}
+		else
+		{
+			Debug.Log ("Tie!");
+			// TODO: disaply tie game
+		}
+	}
+
+}
 
